455048f1300fca60c2f98f7eabfa90a23cc45d55
OniMinetods <146983705+OniMinetods@users.noreply.github.com>
2025-04-09 23:22:33 +0300

    feat: added footer and header

---
+++ lab/src/res/elements/footer/footer.js
@@ -0,0 +1,19 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <p>Левый блок футера</p>
+      </div>
+      <div>
+        <p>Центральный блок футера</p>
+      </div>
+      <div>
+        <p>Правый блок футера</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ lab/src/res/elements/header/header.js
@@ -0,0 +1,19 @@
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <div>
+        <p>Левый блок хедера</p>
+      </div>
+      <div className="div">
+        <p>Средний блок хедера</p>
+      </div>
+      <div className="div">
+        <p>Правый блок хедера</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
